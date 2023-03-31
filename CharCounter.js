eb1a35aa862aa00e0e994d686496c9da33a8036c
Vivek Mathur <71319149+Grimoors@users.noreply.github.com>
2023-03-31 17:19:52 +0000

    Debugging

--- CharCounter.js
+++ CharCounter.js
@@ -13,15 +13,17 @@
 
 // Begin the js script
 
-// get the tweet box and the counter element
+// Get the tweet box element from the DOM
 const tweetBox = document.getElementById("tweet");
+
+// Get the counter element from the DOM
 const counter = document.getElementById("Counter");
 
-// add an event listener to the tweet box that listens for changes in its value
+// Add an event listener to the tweet box element to count the characters
 tweetBox.addEventListener("input", () => {
-  // get the current character count
-  const count = tweetBox.value.length;
+  // Get the number of characters in the tweet box
+  const charCount = tweetBox.value.length;
 
-  // update the character count in the counter element
-  counter.textContent = `${count}/280`;
+  // Update the counter element with the number of characters
+  counter.innerHTML = `<h6>${charCount}/280</h6>`;
 });
